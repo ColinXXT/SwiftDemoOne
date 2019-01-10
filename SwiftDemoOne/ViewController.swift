f56781c130f602d0a0245983df6a93bbe0d03d7b
ColinXXT <h2uimzk9p@126.com>
2019-01-10 14:11:06 +0800

    Initial Commit

---
+++ SwiftDemoOne/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftDemoOne
+//
+//  Created by HSBC Mobile on 10/1/19.
+//  Copyright © 2019年 Demo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
